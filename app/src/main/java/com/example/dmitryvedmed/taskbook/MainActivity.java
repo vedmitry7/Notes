5dff9f9a4dccec4e6662c3d3123a3f3dea75bca0
Vedmitry7 <vedmitry7@gmail.com>
2017-02-22 20:26:55 +0300

    save in pref test

--- app/src/main/java/com/example/dmitryvedmed/taskbook/MainActivity.java
+++ app/src/main/java/com/example/dmitryvedmed/taskbook/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dmitryvedmed.taskbook;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,14 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends ListActivity  {
+
+    public static final String NAME_PREFERENCES = "prefs";
+    SharedPreferences sharedPreferences;
+    ArrayList<String> tasks;
+    MyArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +30,20 @@ public class MainActivity extends ListActivity  {
                 "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
                 "Linux", "OS/2" };
 
-        MyArrayAdapter adapter = new MyArrayAdapter(this, values);
+        loadPreferences();
+        adapter = new MyArrayAdapter(this, values);
+        adapter.setNotifyOnChange(true);
         setListAdapter(adapter);
         //setContentView(R.layout.activity_main);
+    }
+
+    private void loadPreferences(){
+        sharedPreferences = this.getSharedPreferences(NAME_PREFERENCES, Context.MODE_PRIVATE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -33,6 +52,7 @@ public class MainActivity extends ListActivity  {
         Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
 
         Intent intent = new Intent(getApplicationContext(), TaskActivity.class);
+        intent.putExtra("pos", position);
         intent.putExtra("head", item);
         intent.putExtra("text", item + item + item);
         startActivity(intent);
@@ -44,7 +64,7 @@ public class MainActivity extends ListActivity  {
         private final Context context;
         private final String[] values;
 
-        public MyArrayAdapter(Context context, String[] values) {
+        MyArrayAdapter(Context context, String[] values) {
             super(context,  R.layout.rowlayout, R.id.label, values);
             this.context = context;
             this.values = values;
@@ -55,12 +75,19 @@ public class MainActivity extends ListActivity  {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
 
+           // linearLayout.setBackgroundResource(R.drawable.task_shape);
             TextView textView = (TextView) rowView.findViewById(R.id.label);
             TextView textView1 = (TextView) rowView.findViewById(R.id.label1);
-            textView1.setText(values[position]);
-            textView.setText(values[position]+ " " + values[position] + " " + values[position]);
+
+
+            textView1.setText(sharedPreferences.getString(String.valueOf(position)+"0","ХЗ заголовок"));
+            textView.setText(sharedPreferences.getString(String.valueOf(position)+"1","ХЗ текст"));
             // Изменение иконки для Windows и iPhone
             String s = values[position];
+            System.out.println("LOAD ITEM");
+            System.out.println("pos = " + position);
+            System.out.println(sharedPreferences.getString(String.valueOf(position)+"0"," ---0"));
+            System.out.println(sharedPreferences.getString(String.valueOf(position)+"1"," ---1"));
 
             return rowView;
         }
--- app/src/main/java/com/example/dmitryvedmed/taskbook/TaskActivity.java
+++ app/src/main/java/com/example/dmitryvedmed/taskbook/TaskActivity.java
@@ -1,13 +1,19 @@
 package com.example.dmitryvedmed.taskbook;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.EditText;
+
+import static com.example.dmitryvedmed.taskbook.MainActivity.NAME_PREFERENCES;
 
 public class TaskActivity extends AppCompatActivity {
 
-    TextView head,text;
+    EditText head,text;
+    SharedPreferences sharedPreferences;
+    int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,15 +21,36 @@ public class TaskActivity extends AppCompatActivity {
 
         initView();
 
+
     }
     private void initView() {
-        head = (TextView) findViewById(R.id.TaskHeadLineText);
-        text = (TextView) findViewById(R.id.taskText);
+        head = ( EditText) findViewById(R.id.headEditText);
+        text = ( EditText) findViewById(R.id.taskEditText);
 
         Intent intent = getIntent();
         String headText = intent.getStringExtra("head");
         String textText = intent.getStringExtra("text");
+        position = intent.getIntExtra("pos",42);
         head.setText(headText);
         text.setText(textText);
+    }
+
+    private void savePreferences(){
+        sharedPreferences = this.getSharedPreferences(NAME_PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(String.valueOf(position)+"0", String.valueOf(head.getText()));
+        editor.putString(String.valueOf(position)+"1", String.valueOf(text.getText()));
+        editor.commit();
+        System.out.println("SAVE PREF");
+        System.out.println("pos = " + position);
+        System.out.println(sharedPreferences.getString(String.valueOf(position)+"0"," ---0"));
+        System.out.println(sharedPreferences.getString(String.valueOf(position)+"1"," ---1"));
+    }
+
+    @Override
+    protected void onPause() {
+        savePreferences();
+        super.onPause();
     }
 }
