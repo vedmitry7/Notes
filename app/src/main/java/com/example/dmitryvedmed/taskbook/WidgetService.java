620f83dfb4c42ca7f06a26bf53dc9e48cef0d412
Vedmitry7 <vedmitry7@gmail.com>
2017-04-10 21:03:33 +0300

    add Widget with listView

---
+++ app/src/main/java/com/example/dmitryvedmed/taskbook/WidgetService.java
@@ -0,0 +1,13 @@
+package com.example.dmitryvedmed.taskbook;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class WidgetService extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return(new WidgetViewFactory(this.getApplicationContext(),
+                intent));
+    }
+}
