cd701e9478f70df54f7cd5897c410e0323ffb583
Vedmitry7 <vedmitry7@gmail.com>
2017-05-10 20:12:45 +0300

    set padding in dialog. set dimBehind dialog. improve deliver behavior in listTaskDialog. set size of cardViewListTask elements. set checkbox image position add edit button in dialog

--- app/src/main/java/com/example/dmitryvedmed/taskbook/ui/SettingsActivity.java
+++ app/src/main/java/com/example/dmitryvedmed/taskbook/ui/SettingsActivity.java
@@ -4,19 +4,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.dmitryvedmed.taskbook.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    TextView taskFontLabel, cardFontLabel, taskFontValue, cardFontValue;
+    SeekBar taskFontSeekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        onCreatePreferenceActivity();
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.setting_toolbar);
         setSupportActionBar(toolbar);
        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        taskFontLabel = (TextView) findViewById(R.id.taskFont);
+        cardFontLabel = (TextView) findViewById(R.id.cardFont);
 
     }
 
@@ -27,8 +35,11 @@ public class SettingsActivity extends AppCompatActivity {
         return true;
     }
 
-    @SuppressWarnings("deprecation")
-    private void onCreatePreferenceActivity() {
-       // addPreferencesFromResource(R.xml.pref_general);
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.taskFont:
+
+        }
     }
 }
